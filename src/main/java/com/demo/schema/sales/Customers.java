1797325ff94e7e759a826d38932b1f37bf370af7
Moises <moises.veliz.francia@gmail.com>
2017-04-05 09:15:24 -0500

    adecuacion

--- src/main/java/com/demo/schema/sales/Customers.java
+++ src/main/java/com/demo/schema/sales/Customers.java
@@ -2,7 +2,7 @@
 // Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.2.7 
 // Visite <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2017.04.04 a las 02:33:17 PM COT 
+// Generado el: 2017.04.04 a las 05:26:20 PM COT 
 //
 
 
