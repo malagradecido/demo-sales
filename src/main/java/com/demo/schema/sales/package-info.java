155a60a9fa73d0853a7c953189b319569b9e1a5f
Moises Veliz <moises.veliz.francia@gmail.com>
2017-04-04 23:01:30 -0500

    preparando pruebas para enviar y recibir xml en dao

--- src/main/java/com/demo/schema/sales/package-info.java
+++ src/main/java/com/demo/schema/sales/package-info.java
@@ -2,7 +2,7 @@
 // Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.2.7 
 // Visite <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2017.04.04 a las 02:33:17 PM COT 
+// Generado el: 2017.04.04 a las 09:49:14 PM PET 
 //
 
 @javax.xml.bind.annotation.XmlSchema(namespace = "http://demo.com/schema/sales", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
