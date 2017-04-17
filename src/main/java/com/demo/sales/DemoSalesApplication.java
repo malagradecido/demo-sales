b4f11d8eafe3fe892df8f11a031067417f586cb7
Moises Veliz <moises.veliz.francia@gmail.com>
2017-04-17 00:12:22 -0500

    desarrollando configuración de colas

--- src/main/java/com/demo/sales/DemoSalesApplication.java
+++ src/main/java/com/demo/sales/DemoSalesApplication.java
@@ -1,18 +1,20 @@
 package com.demo.sales;
 
-import org.apache.log4j.Logger;
-import org.springframework.boot.CommandLineRunner;
+//import org.apache.log4j.Logger;
+//import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-import com.demo.sales.bean.CustomerBean;
-import com.demo.sales.dao.ISalesDAO;
+//import com.demo.sales.bean.CustomerBean;
+//import com.demo.sales.dao.ISalesDAO;
 
 @SpringBootApplication
+@EnableScheduling
 public class DemoSalesApplication {
 	
-	private static final Logger logger = Logger.getLogger(DemoSalesApplication.class);
+//	private static final Logger logger = Logger.getLogger(DemoSalesApplication.class);
 	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoSalesApplication.class, args);
@@ -25,14 +27,14 @@ public class DemoSalesApplication {
 		return builder.sources(DemoWsProducerApplication.class);
 	}*/
 	
-	@Bean
-	CommandLineRunner lookup(ISalesDAO iSalesDAO) {
-		return args -> {
-			//iSalesDAO.viewArray();
-			CustomerBean customerDTO = new CustomerBean(1);
-			customerDTO = iSalesDAO.getCustomer(customerDTO);
-			logger.info(customerDTO);
-			//System.err.println(response.getGetQuoteResult());
-		};
-	}
+//	@Bean
+//	CommandLineRunner lookup(ISalesDAO iSalesDAO) {
+//		return args -> {
+//			//iSalesDAO.viewArray();
+//			CustomerBean customerDTO = new CustomerBean(1);
+//			customerDTO = iSalesDAO.getCustomer(customerDTO);
+//			logger.info(customerDTO);
+//			//System.err.println(response.getGetQuoteResult());
+//		};
+//	}
 }
---
+++ src/main/java/com/demo/sales/config/RabbitConfig.java
@@ -0,0 +1,79 @@
+package com.demo.sales.config;
+
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.backoff.ExponentialBackOffPolicy;
+import org.springframework.retry.support.RetryTemplate;
+
+import com.demo.sales.amqp.receiver.DemoReceiver;
+
+@Configuration
+public class RabbitConfig {
+	
+	public final static String queueName = "spring-boot";
+	
+	@Bean
+    public ConnectionFactory connectionFactory() {
+        return new CachingConnectionFactory("localhost");
+    }
+
+    @Bean
+    public AmqpAdmin amqpAdmin() {
+        return new RabbitAdmin(connectionFactory());
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate() {
+    	
+    	RabbitTemplate template = new RabbitTemplate(connectionFactory());
+    	RetryTemplate retryTemplate = new RetryTemplate();
+    	ExponentialBackOffPolicy backOffPolicy = new ExponentialBackOffPolicy();
+    	backOffPolicy.setInitialInterval(500);
+    	backOffPolicy.setMultiplier(10.0);
+    	backOffPolicy.setMaxInterval(10000);
+    	retryTemplate.setBackOffPolicy(backOffPolicy);
+    	template.setRetryTemplate(retryTemplate);
+        return template;
+    }
+
+	@Bean
+    Queue queue() {
+        return new Queue(queueName, false);
+    }
+
+    @Bean
+    TopicExchange exchange() {
+        return new TopicExchange("spring-boot-exchange");
+    }
+
+    @Bean
+    Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(queueName);
+    }
+
+    @Bean
+    SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
+            MessageListenerAdapter listenerAdapter) {
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.setQueueNames(queueName);
+        container.setMessageListener(listenerAdapter);
+        return container;
+    }
+
+    @Bean
+    MessageListenerAdapter listenerAdapter(DemoReceiver receiver) {
+        return new MessageListenerAdapter(receiver, "receiveMessage");
+    }
+}
