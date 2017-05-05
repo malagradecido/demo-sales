8375b249e867681cc913d127841902e1993f8be2
Moises Veliz <moises.veliz.francia@gmail.com>
2017-05-04 22:48:52 -0500

    Finalizado desarrollo de remoting

--- src/main/java/com/demo/sales/config/RemotingConfig.java
+++ src/main/java/com/demo/sales/config/RemotingConfig.java
@@ -1,22 +1,20 @@
 package com.demo.sales.config;
 
-import java.util.Properties;
-
-import org.springframework.boot.autoconfigure.web.DispatcherServletAutoConfiguration;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.remoting.caucho.HessianServiceExporter;
+import org.springframework.remoting.httpinvoker.HttpInvokerServiceExporter;
 import org.springframework.remoting.rmi.RmiServiceExporter;
-import org.springframework.web.servlet.DispatcherServlet;
-import org.springframework.web.servlet.HandlerMapping;
-import org.springframework.web.servlet.handler.SimpleUrlHandlerMapping;
 
 import com.demo.sales.services.ISalesService;
 
 @Configuration
 public class RemotingConfig {
 
+	@Autowired
+	ISalesService salesService;
+	
 	@Bean
 	public RmiServiceExporter rmiSalesService(ISalesService salesService) {
 		RmiServiceExporter rmiServiceExporter = new RmiServiceExporter();
@@ -26,35 +24,18 @@ public class RemotingConfig {
 		rmiServiceExporter.setRegistryPort(1199);
 		return rmiServiceExporter;
 	}
-
-//	@Bean
-//	public DispatcherServlet dispatcherServlet() {
-//		return new DispatcherServlet();
-//	}
-//
-//	@Bean
-//	public ServletRegistrationBean dispatcherServletRegistration() {
-//		ServletRegistrationBean registration = new ServletRegistrationBean(dispatcherServlet());
-//		registration.setLoadOnStartup(1);
-//		registration.setName(
-//				DispatcherServletAutoConfiguration.
-//				DEFAULT_DISPATCHER_SERVLET_REGISTRATION_BEAN_NAME);
-//		registration.addUrlMappings("/", "*.service");
-//		return registration;
-//	}
 	
-//	@Bean
-//	public HandlerMapping hessianMapping() {
-//		SimpleUrlHandlerMapping mapping = new SimpleUrlHandlerMapping();
-//		Properties mappings = new Properties();
-//		mappings.setProperty("/sales.service", "hessianExportedSalesService");
-//		mapping.setMappings(mappings);
-//		return mapping;
-//	}
-	
-	@Bean(name = "salesService")
-	public HessianServiceExporter hessianExportedSalesService(ISalesService salesService) {
+	@Bean(name = "/HessianSalesService")
+	public HessianServiceExporter hessianExportedSalesService() {
 		HessianServiceExporter exporter = new HessianServiceExporter();
+		exporter.setService(salesService);
+		exporter.setServiceInterface(ISalesService.class);
+		return exporter;
+	}
+	
+	@Bean(name = "/HttpInvokerSalesService")
+	public HttpInvokerServiceExporter httpInvokerExportedSalesService() {
+		HttpInvokerServiceExporter exporter = new HttpInvokerServiceExporter();
 		exporter.setService(salesService);
 		exporter.setServiceInterface(ISalesService.class);
 		return exporter;
